73a9712cccbbcce4ff26edb22c88d24dcf69ae48
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-09 00:26:24 +0530

    async http

--- worldtime/lib/pages/choose_location.dart
+++ worldtime/lib/pages/choose_location.dart
@@ -6,38 +6,15 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
-  void getDate() async {
-    String str1 = await Future.delayed(Duration(seconds: 3), () {
-      return "Hmmmmm delayed.";
-    });
-    String str2 = await Future.delayed(Duration(seconds: 2), () {
-      return "booooo delayed.";
-    });
-    print("statement : $str1-$str2");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getDate();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: Text("Choose a Location."),
-          centerTitle: true,
-        ),
-        body: RaisedButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
-        ));
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose a Location."),
+        centerTitle: true,
+      ),
+    );
   }
 }
