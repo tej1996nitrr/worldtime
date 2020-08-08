ab5cc80d7489d1c60aef0e6578a2349878b1d4f0
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-08 23:45:33 +0530

    ASYC

--- worldtime/lib/pages/choose_location.dart
+++ worldtime/lib/pages/choose_location.dart
@@ -7,9 +7,20 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
+  void getDate() async{
+    await Future.delayed(Duration(seconds: 3), () {
+      print("Hmmmmm delayed.");
+    });
+    Future.delayed(Duration(seconds: 2), () {
+      print("booooo delayed.");
+    });
+    print("statement")
+  }
+
   @override
   void initState() {
     super.initState();
+    getDate();
   }
 
   @override
