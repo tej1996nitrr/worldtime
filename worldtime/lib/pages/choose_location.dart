350606a3575934b4eed1fef1121e03a1c17c38f8
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-08 23:25:53 +0530

    widget lifecycle

--- worldtime/lib/pages/choose_location.dart
+++ worldtime/lib/pages/choose_location.dart
@@ -6,16 +6,27 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text("Choose a Location."),
-        centerTitle: true,
-      ),
-      body: Text("Choose Location Screen"),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text("Choose a Location."),
+          centerTitle: true,
+        ),
+        body: RaisedButton(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+        ));
   }
 }
