1611aafe77d11d6ba62c4657730144285885703a
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-09 14:14:05 +0530

    passing route data

--- worldtime/lib/pages/loading.dart
+++ worldtime/lib/pages/loading.dart
@@ -12,9 +12,13 @@ class _LoadingState extends State<Loading> {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
     setState(() {
       time = instance.time;
+    });
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
   }
 
@@ -29,7 +33,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(50),
-        child: Text(time),
+        child: Text('loading...'),
       ),
     );
   }
